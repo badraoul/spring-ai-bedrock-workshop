925a1e10715ec5c16c2c583e96ef1295ea0dca8f
badraoul <steve.watkins@broadcom.com>
2024-04-12 17:14:56 -0700

    initial upload
    upload local files

---
+++ src/main/java/com/xkcd/ai/helloworld/SimpleAiController.java
@@ -0,0 +1,28 @@
+package com.xkcd.ai.helloworld;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.bedrock.cohere.BedrockCohereChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class SimpleAiController {
+
+	private final ChatClient chatClient;
+
+	@Autowired
+	public SimpleAiController(ChatClient chatClient) {
+		this.chatClient = chatClient;
+	}
+
+	@GetMapping("/ai/simple")
+	public Map<String, String> generation(
+			@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+		return Map.of("generation", chatClient.call(message));
+	}
+
+}
